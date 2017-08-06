d07c8d771a8139050ae2e8585dfa9467a1e82a0d
Szekeres Bálint <szekeres.balint@hps.hu>
2017-08-06 12:07:54 +0200

    course_reference_course_block table

---
+++ database/migrations/21____create_course_reference_course_block_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCourseReferenceCourseBlockTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_reference_course_block', function (Blueprint $table) {
+            $table->integer('course_id')->unsigned();
+            $table->foreign('course_id')->references('id')->on('courses');
+
+            $table->integer('course_block_id')->unsigned();
+            $table->foreign('course_block_id')->references('id')->on('course_blocks');
+
+            $table->primary(['course_id', 'course_block_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course_reference_course_block');
+    }
+}
