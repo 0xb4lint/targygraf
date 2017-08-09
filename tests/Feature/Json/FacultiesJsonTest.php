c82799fef6cc18a4875eaaf7b5efd8c1483a14a7
Szekeres Bálint <szekeres.balint@hps.hu>
2017-08-09 12:45:17 +0200

    test json files ancestors

--- tests/Feature/Json/FacultiesJsonTest.php
+++ tests/Feature/Json/FacultiesJsonTest.php
@@ -5,4 +5,17 @@ namespace Tests\Feature\Json;
 class FacultiesJsonTest extends AbstractJsonTest
 {
     protected $directory = 'json/faculties';
+    protected $universitiesDirectory = 'json/universities';
+
+    public function testUniversityExists()
+    {
+        $files = scandir(base_path($this->directory));
+
+        foreach ($files as $file) {
+            if ($file[0] != '.') {
+                $fileParts = explode('_', pathinfo($file, PATHINFO_FILENAME));
+                $this->assertTrue(file_exists(base_path($this->universitiesDirectory.'/'.$fileParts[0].'.json')), $file);
+            }
+        }
+    }
 }
--- tests/Feature/Json/ProgramsJsonTest.php
+++ tests/Feature/Json/ProgramsJsonTest.php
@@ -5,4 +5,17 @@ namespace Tests\Feature\Json;
 class ProgramsJsonTest extends AbstractJsonTest
 {
     protected $directory = 'json/programs';
+    protected $facultiesDirectory = 'json/faculties';
+
+    public function testFacultyExists()
+    {
+        $files = scandir(base_path($this->directory));
+
+        foreach ($files as $file) {
+            if ($file[0] != '.') {
+                $fileParts = explode('_', pathinfo($file, PATHINFO_FILENAME));
+                $this->assertTrue(file_exists(base_path($this->facultiesDirectory.'/'.$fileParts[0].'_'.$fileParts[1].'.json')), $file);
+            }
+        }
+    }
 }
