c3acd7c4115d45420c883a6ccf2caf3a67113e77
Szekeres Bálint <szekeres.balint@hps.hu>
2019-02-17 00:35:01 +0100

    new deployment method

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-eval "$(ssh-agent -s)"
-base64 --decode <<< "$DEPLOY_PRIVATE_KEY" > deploy_key
-chmod 600 deploy_key
-ssh-add deploy_key
-ssh -o "StrictHostKeyChecking no" -l root szekeres.me \
-	"sudo -u www-data -H sh -c ' \
+ssh -l www-data targygraf.hu \
+	"sh -c ' \
 	cd /var/www/targygraf.hu; \
 	rm composer.lock; \
 	git pull origin master && \
