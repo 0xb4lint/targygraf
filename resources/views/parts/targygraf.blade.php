97b225f6a00a39c770c09ab813a31506c24505b3
Szekeres Bálint <szekeres.balint@hps.hu>
2019-02-17 18:10:51 +0100

    tab fixes

--- resources/views/parts/targygraf.blade.php
+++ resources/views/parts/targygraf.blade.php
@@ -1,42 +1,40 @@
 <?php $lastRow = 0; ?>
 <div class="content" data-specialis="0">
-@foreach ($program->courseBlocks as $courseBlock)
+@foreach ($program->courseBlocks as $row => $courseBlock)
+	@if ( $courseBlock->row != $lastRow )
+		</div>
+		<div class="content" data-specialis="1">
+		<?php $lastRow = $courseBlock->row; ?>
+	@endif
 
-    @if ( $courseBlock->row != $lastRow )
-        </div>
-        <div class="content" data-specialis="1">
-        <?php $lastRow = $courseBlock->row; ?>
-    @endif
+	<?php $sumCredits = 0; ?>
+	<div class="course-block"
+		data-id="{{ $courseBlock->getPaddedID() }}"
+		data-is-counted="{{ $courseBlock->is_counted }}">
+		<div class="course-block-title">{!! $courseBlock->getName() !!}</div>
+		@foreach ($courseBlock->courses as $course)
+			<?php $sumCredits += $course->credits; ?>
 
-    <?php $sumCredits = 0; ?>
-    <div class="course-block"
-        data-id="{{ $courseBlock->getPaddedID() }}"
-        data-is-counted="{{ $courseBlock->is_counted }}">
-        <div class="course-block-title">{!! $courseBlock->getName() !!}</div>
-        @foreach ($courseBlock->courses as $course)
-            <?php $sumCredits += $course->credits; ?>
+			@if ($course->code == '______')
+				<hr>
+			@else
+				<div class="course"
+					title="{{{ $course->getTitle() }}}"
+					data-id="{{ $course->getPaddedID() }}"
+					data-code="{{ $course->code }}"
+					data-credits="{{ $course->credits }}"
+					data-referenced-course-blocks="{{ implode(',', $course->getCourseBlockReferencesIDs()) }}"
+					data-prerequisites="{{ implode(',', $course->getPrerequisitesIDs()) }}">
+					{{ $course->name }}
+				</div>
+			@endif
+		@endforeach
 
-            @if ($course->code == '______')
-                <hr>
-            @else
-                <div class="course"
-                    title="{{{ $course->getTitle() }}}"
-                    data-id="{{ $course->getPaddedID() }}"
-                    data-code="{{ $course->code }}"
-                    data-credits="{{ $course->credits }}"
-                    data-referenced-course-blocks="{{ implode(',', $course->getCourseBlockReferencesIDs()) }}"
-                    data-prerequisites="{{ implode(',', $course->getPrerequisitesIDs()) }}">
-                    {{ $course->name }}
-                </div>
-            @endif
-        @endforeach
-
-        @if ( !$courseBlock->row && $sumCredits )
-            <div class="muted">
-                ∑ {{ $sumCredits }} kredit
-            </div>
-        @endif
-
-    </div>
+		@if ( !$courseBlock->row && $sumCredits )
+			<div class="muted">
+				∑ {{ $sumCredits }} kredit
+			</div>
+		@endif
+	</div>
 @endforeach
 </div>
